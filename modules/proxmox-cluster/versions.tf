a2649287c3a1abd213dfe66251e216ad54087f45
Bharat Baliyan <bharat.baliyan19@gmail.com>
2026-08-09 00:16:39 +1000

    proxmox-cluster: declare bpg/proxmox in required_providers

--- modules/proxmox-cluster/versions.tf
+++ modules/proxmox-cluster/versions.tf
@@ -2,6 +2,10 @@
 terraform {
   required_version = ">= 1.12.0"
   required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "~> 0.110"
+    }
     dns = {
       source                = "hashicorp/dns"
       version               = "~> 3.6"
